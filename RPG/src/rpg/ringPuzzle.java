531f850c44772ac9bbf6708fe914559e213e11a0
epowell50 <Epowell50@gmail.com>
2019-11-18 23:04:54 -0500

    Ring puzzle almost finished, need to work out some logic stuff

--- RPG/src/rpg/ringPuzzle.java
+++ RPG/src/rpg/ringPuzzle.java
@@ -8,6 +8,8 @@ package rpg;
 import java.io.FileNotFoundException;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -46,15 +48,18 @@ public class ringPuzzle extends Application {
     ImageView pillar3Ring4 = new ImageView();
     
     // Labels for things
-    Label ringSelection = new Label();
-    Label pillarSelection = new Label();
-    Label display = new Label();
+    Label ringSelection = new Label("");
+    Label pillarSelection = new Label("");
+    Label display = new Label("");
     
     // Variables to keep track of ring locations (Pillar wise)
     int redRingLoc = 1;
     int blueRingLoc = 1;
     int greenRingLoc = 1;
     int yellowRingLoc = 1;
+    
+    // Direction currently on
+    int directionNumber = 0;
     
     // Ring selection variable
     String ringSelected = "";
@@ -100,19 +105,8 @@ public class ringPuzzle extends Application {
         pillar3Ring4.setFitHeight(25);
         
         // Setting start point and giving directions
-        Label description = new Label ("In front of you are several pillars with "
+        Label description = new Label ("In front of you are several pillars with"
                 + " different colored rings attached.");
-        Label promptRed = new Label ("The red ring can be moved to any pillar"
-                + " without consequence.");
-        Label promptBlue = new Label ("The blue ring can only safely be moved "
-                + " to a pillar as long as the red ring is not "
-                + " present on that pillar.");
-        Label promptGreen = new Label ("The green ring can only safely be moved"
-                + " to a pillar that has no rings, or only a yellow ring");
-        Label promptYellow = new Label ("The yellow ring can only safely be "
-                + " moved to a pillar that has no rings");
-        Label prompt = new Label ("'It seems I need to move all the rings"
-                + " to the rightmost pillar'");
         
         ringSelection.setText("");
         pillarSelection.setText("");
@@ -121,29 +115,43 @@ public class ringPuzzle extends Application {
         
         // Buttons to select the rings
         Button selectRed = new Button("Select Red Ring");
+        selectRed.setOnAction(new selectRedHandler());
         Button selectBlue = new Button("Select Blue Ring");
+        selectBlue.setOnAction(new selectBlueHandler());
         Button selectYellow = new Button("Select Yellow Ring");
+        selectYellow.setOnAction(new selectYellowHandler());
         Button selectGreen = new Button("Select Green Ring");
+        selectGreen.setOnAction(new selectGreenHandler());
         
         // Buttons to select pillars
         Button pillar1 = new Button("Select Pillar 1");
+        pillar1.setOnAction(new selectPillar1Handler());
         Button pillar2 = new Button("Select Pillar 2");
+        pillar2.setOnAction(new selectPillar2Handler());
         Button pillar3 = new Button("Select Pillar 3");
+        pillar3.setOnAction(new selectPillar3Handler());
         
         // Move button
         Button move = new Button("Move");
+        move.setOnAction(new moveHandler());
         
         // Leave button
         Button leave = new Button("Leave");
+        leave.setOnAction(e-> {
+            primaryStage.close();
+        });
         
         // Reset button
         Button reset = new Button("Reset Rings");
+        reset.setOnAction(new resetHandler());
         
         // Submit button
         Button submit = new Button("Submit arrangement");
+        submit.setOnAction(new submitHandler());
         
         // Get directions button
         Button getDirections = new Button ("Get directions");
+        getDirections.setOnAction(new directionHandler());
         
         // First some boxes to hold the images
         
@@ -195,8 +203,8 @@ public class ringPuzzle extends Application {
         primaryStage.initModality(Modality.APPLICATION_MODAL);
         scene.getStylesheets().add("rpg-styles.css");
         
-        primaryStage.setTitle("A series of pillars with "
-                + " several rings attached");
+        primaryStage.setTitle("A series of pillars with"
+                + " several rings attached.");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
@@ -206,11 +214,319 @@ public class ringPuzzle extends Application {
     }
     
     public void resetRings() {
-        pillar1Ring1.setImage(redRing);
-        pillar1Ring2.setImage(blueRing);
-        pillar1Ring3.setImage(yellowRing);
-        pillar1Ring4.setImage(greenRing);
+        
+        // Resetting ring values
+        redRingLoc = 1;
+        blueRingLoc = 1;
+        yellowRingLoc = 1;
+        greenRingLoc = 1;
+        
+        checkImg();
         
         return;
+    }
+    
+    public void checkImg() {
+        
+        // Red ring check
+        if(redRingLoc == 1)
+        {
+            pillar1Ring1.setImage(redRing);
+            pillar2Ring1.setImage(null);
+            pillar3Ring1.setImage(null);
+        }
+        else if (redRingLoc == 2)
+        {
+            pillar1Ring1.setImage(null);
+            pillar2Ring1.setImage(redRing);
+            pillar3Ring1.setImage(null);
+        }
+        else if (redRingLoc == 3)
+        {
+            pillar1Ring1.setImage(null);
+            pillar2Ring1.setImage(null);
+            pillar3Ring1.setImage(redRing);
+        }
+        
+        // Blue ring check
+        if(blueRingLoc == 1)
+        {
+            pillar1Ring2.setImage(blueRing);
+            pillar2Ring2.setImage(null);
+            pillar3Ring2.setImage(null);
+        }
+        else if (blueRingLoc == 2)
+        {
+            pillar1Ring2.setImage(null);
+            pillar2Ring2.setImage(blueRing);
+            pillar3Ring2.setImage(null);
+        }
+        else if (blueRingLoc == 3)
+        {
+            pillar1Ring2.setImage(null);
+            pillar2Ring2.setImage(null);
+            pillar3Ring2.setImage(blueRing);
+        }
+        
+        // Green ring check
+        if(greenRingLoc == 1)
+        {
+            pillar1Ring3.setImage(greenRing);
+            pillar2Ring3.setImage(null);
+            pillar3Ring3.setImage(null);
+        }
+        else if (greenRingLoc == 2)
+        {
+            pillar1Ring3.setImage(null);
+            pillar2Ring3.setImage(greenRing);
+            pillar3Ring3.setImage(null);
+        }
+        else if (greenRingLoc == 3)
+        {
+            pillar1Ring3.setImage(null);
+            pillar2Ring3.setImage(null);
+            pillar3Ring3.setImage(greenRing);
+        }
+        
+        // Yellow ring check
+        if(yellowRingLoc == 1)
+        {
+            pillar1Ring4.setImage(yellowRing);
+            pillar2Ring4.setImage(null);
+            pillar3Ring4.setImage(null);
+        }
+        else if (yellowRingLoc == 2)
+        {
+            pillar1Ring4.setImage(null);
+            pillar2Ring4.setImage(yellowRing);
+            pillar3Ring4.setImage(null);
+        }
+        else if (yellowRingLoc == 3)
+        {
+            pillar1Ring4.setImage(null);
+            pillar2Ring4.setImage(null);
+            pillar3Ring4.setImage(yellowRing);
+        }
+    }
+    
+    public boolean checkAnswer() {
+        if(redRingLoc == 3 && blueRingLoc == 3 && greenRingLoc == 3 && 
+                yellowRingLoc == 3)
+        {
+            solved = true;
+            display.setText("Youd did it!");
+        }
+        else
+        {
+            display.setText("Something is not quite right...");
+        }
+        
+        return solved;
+    }
+    
+    public boolean getSolved() {
+        return solved;
+    }
+    
+    public boolean redLogic() {
+        
+        if(ringSelected == "RED" && pillarSelected == "PILLAR 1")
+            redRingLoc = 1;
+        if(ringSelected == "RED" && pillarSelected == "PILLAR 2")
+            redRingLoc = 2;
+        if(ringSelected == "RED" && pillarSelected == "PILLAR 3")
+            redRingLoc = 3;
+        
+        checkImg();
+        
+        return true;
+    }
+    
+    public boolean blueLogic() {
+        
+        if(ringSelected == "BLUE" && pillarSelected == "PILLAR 1")
+            blueRingLoc = 1;
+        if(ringSelected == "BLUE" && pillarSelected == "PILLAR 2")
+            blueRingLoc = 2;
+        if(ringSelected == "BLUE" && pillarSelected == "PILLAR 3")
+            blueRingLoc = 3;
+        
+        checkImg();
+        
+        return true;
+    }
+    
+    public boolean greenLogic() {
+        
+        if(ringSelected == "GREEN" && pillarSelected == "PILLAR 1")
+            greenRingLoc = 1;
+        if(ringSelected == "GREEN" && pillarSelected == "PILLAR 2")
+            greenRingLoc = 2;
+        if(ringSelected == "GREEN" && pillarSelected == "PILLAR 3")
+            greenRingLoc = 3;
+        
+        checkImg();
+        
+        return true;
+    }
+    
+    public boolean yellowLogic() {
+        
+        if(ringSelected == "YELLOW" && pillarSelected == "PILLAR 1")
+            yellowRingLoc = 1;
+        if(ringSelected == "YELLOW" && pillarSelected == "PILLAR 2")
+            yellowRingLoc = 2;
+        if(ringSelected == "YELLOW" && pillarSelected == "PILLAR 3")
+            yellowRingLoc = 3;
+        
+        checkImg();
+        
+        return true;
+    }
+    
+    public void puzzleLogic() {
+            // Todo stuff here...
+            return;
+    }
+    
+    class directionHandler implements EventHandler<ActionEvent> {
+        
+        @Override
+        public void handle (ActionEvent event) {
+            if(directionNumber == 0)
+            {
+                display.setText("The red ring can be moved to any pillar"
+                + " without consequence.");
+                directionNumber++;
+            }
+            else if(directionNumber == 1)
+            {
+                display.setText("The blue ring can only safely be moved"
+                + " to a pillar as long as the red ring is not"
+                + " present on that pillar.");
+                directionNumber++;
+            }
+            else if(directionNumber == 2)
+            {
+                display.setText("The green ring can only safely be moved"
+                + " to a pillar that has no rings, or only a yellow ring.");
+                directionNumber++;
+            }
+            else if(directionNumber == 3)
+            {
+                display.setText("The yellow ring can only safely be"
+                + " moved to a pillar that has no rings.");
+                directionNumber++;
+            }
+            else if(directionNumber == 4)
+            {
+                display.setText("'It seems I need to move all the rings"
+                + " to the rightmost pillar.'");
+                directionNumber++;
+            }
+             else if(directionNumber == 5)
+            {
+                display.setText("Rings can only be moved if there are no rings"
+                        + " above the ring you wish to move.");
+                directionNumber++;
+            }
+            else if(directionNumber == 6)
+            {
+                display.setText("");
+                directionNumber = 0;
+            }
+        }
+    }
+    
+    class selectRedHandler implements EventHandler<ActionEvent> {
+        
+        @Override
+        public void handle (ActionEvent event) {
+            ringSelection.setText("Red ring selected");
+            ringSelected = "RED";
+        }
+    }
+    
+    class selectBlueHandler implements EventHandler<ActionEvent> {
+        
+        @Override
+        public void handle (ActionEvent event) {
+            ringSelection.setText("Blue ring selected");
+            ringSelected = "BLUE";
+        }
+    }
+    
+    class selectGreenHandler implements EventHandler<ActionEvent> {
+        
+        @Override
+        public void handle (ActionEvent event) {
+            ringSelection.setText("Green ring selected");
+            ringSelected = "GREEN";
+        }
+    }
+    
+    class selectYellowHandler implements EventHandler<ActionEvent> {
+        
+        @Override
+        public void handle (ActionEvent event) {
+            ringSelection.setText("Yellow ring selected");
+            ringSelected = "YELLOW";
+        }
+    }
+    
+    class selectPillar1Handler implements EventHandler<ActionEvent> {
+        
+        @Override
+        public void handle (ActionEvent event) {
+            pillarSelection.setText("Pillar 1 selected");
+            pillarSelected = "PILLAR 1";
+        }
+    }
+    
+    class selectPillar2Handler implements EventHandler<ActionEvent> {
+        
+        @Override
+        public void handle (ActionEvent event) {
+            pillarSelection.setText("Pillar 2 selected");
+            pillarSelected = "PILLAR 2";
+        }
+    }
+    
+    class selectPillar3Handler implements EventHandler<ActionEvent> {
+        
+        @Override
+        public void handle (ActionEvent event) {
+            pillarSelection.setText("Pillar 3 selected");
+            pillarSelected = "PILLAR 3";
+        }
+    }
+    
+    class submitHandler implements EventHandler<ActionEvent> {
+        
+        @Override
+        public void handle (ActionEvent event) {
+            checkAnswer();
+        }
+    }
+    
+    class resetHandler implements EventHandler<ActionEvent> {
+        
+        @Override
+        public void handle (ActionEvent event) {
+            resetRings();
+        }
+    }
+    
+    class moveHandler implements EventHandler<ActionEvent> {
+        
+        @Override
+        public void handle (ActionEvent event) {
+            redLogic();
+            blueLogic();
+            greenLogic();
+            yellowLogic();
+            
+            puzzleLogic();
+        }
     }
 }
