ce208fa94e55c83afefab8ec5b281cda71597b0b
yeungs2019 <sayeung@csustudent.net>
2019-11-19 22:29:26 -0500

    Weavy's classes with comments

--- RPG/src/rpg/DarkSuit.java
+++ RPG/src/rpg/DarkSuit.java
@@ -9,46 +9,81 @@ import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
  *
  * @author jianc
  */
-public class DarkSuit extends Items {
-    private String effect;
-    private Boolean used = true;
-    private Boolean correctLocation = false;
-    protected Boolean inInventory = false;
+public class DarkSuit extends Application  {
     
-    public DarkSuit (String t, String n, boolean o, int v, 
-            int l, Boolean m, String e, Boolean u, Boolean a)
-    {
-        super(t, n, o, v, l, m);
-        effect = e;
-        used = u;
-        correctLocation = a;
-    }
+    private String effect = "";
+    private Boolean correctLocation = true;
+    protected Boolean inInventory = false;
+    protected String type = "";
+    protected String name = "";
+    protected int value = 0;
+    protected int level = 0;
+    protected Boolean isMagic = false;
+    protected Boolean isUsed = false;
+    // Creates the label
+    Label label = new Label("You have found a jar of purple potion, would"
+                + " you like to pick it up?");
+     // Creates the button
+        Button Yes = new Button();
+        Button No = new Button();
+        Button Used = new Button();
+        Button Unused = new Button();
+    
+//    public Test (String t, String n, boolean o, int v, 
+//            int l, Boolean m, String e, Boolean u, Boolean a)
+//    {
+//        type = t;
+//        name = n;
+//        isInventory = o;
+//        value = v;
+//        level = l;
+//        isMagic = m;
+//        effect = e;
+//        isUsed = u;
+//        correctLocation = a;
+//    }
     
      
     public void start(Stage primaryStage) {
-        // Creates the button
-        Button Yes = new Button();
-        Button No = new Button();
         // sets the text of the button
         Yes.setText("Yes");
         No.setText("No");
+        Used.setText("Use it!");
+        Unused.setText("Save it!");
+   
+        
+        // Leaves the window
+        Button Leave = new Button("Leave");
+        Leave.setOnAction(e-> {
+                primaryStage.close();
+            });
         
         // Register the event handler
         Yes.setOnAction(new YesClickHandler());
         No.setOnAction(new NoClickHandler());
+        Leave.setOnAction(new LeaveClickHandler());
+        Used.setOnAction(new UsedClickHandler());
+        Unused.setOnAction(new UnusedClickHandler());
     
-        HBox ButtonHolder = new HBox(10, Yes, No);
-        Scene scene = new Scene(ButtonHolder, 300, 250);
+        HBox ButtonHolder = new HBox(10, Yes, No, Leave);
+        ButtonHolder.setAlignment(Pos.CENTER);
+        VBox LabelHolder = new VBox(100, label, ButtonHolder);
+        LabelHolder.setAlignment(Pos.CENTER);
+ 
+        Scene scene = new Scene(LabelHolder, 300, 250);
         
         primaryStage.setTitle("DarkSuit");
         primaryStage.setScene(scene);
@@ -79,33 +114,70 @@ public class DarkSuit extends Items {
 //            return false;
 //        }     
 //    }
-    // This dicatates when user decides to use the item
+    // This dicatates when user decides to add the item
     class YesClickHandler implements EventHandler<ActionEvent>
     {
         @Override
         public void handle(ActionEvent event){
-            isUsed();
+            label.setText("You have picked up a 'skip puzzle' potion.");
+            Yes.setVisible(false);
+            No.setVisible(false);
+            
+            addToInventory();
         }       
     }
     class NoClickHandler implements EventHandler<ActionEvent>
     {
         @Override
         public void handle(ActionEvent event){
-            isUsed();
+            label.setText("You have left a 'skip puzzle' potion. Too bad!");
+            Yes.setVisible(false);
+            No.setVisible(false);
+           
+            addToInventory();
+        }
+    }
+    class UsedClickHandler implements EventHandler<ActionEvent>
+    {
+        @Override
+        public void handle(ActionEvent event){
+            label.setText("You will have bypassed this puzzle. Count it as done");
+            Used.setVisible(false);
+            Unused.setVisible(false);
+            
+            addToInventory();
+        }       
+    }
+    class UnusedClickHandler implements EventHandler<ActionEvent>
+    {
+        @Override
+        public void handle(ActionEvent event){
+            label.setText("Saving it? If you insist.");
+            Used.setVisible(false);
+            Unused.setVisible(false);
+           
+            addToInventory();
+        }
+    }
+    class LeaveClickHandler implements EventHandler<ActionEvent>
+    {
+        @Override
+        public void handle(ActionEvent event){
+            addToInventory();
         }
     }
     public void addToInventory(){
         // If the user gets the item, they can choose to add it or not
         if(correctLocation){
             // adds the inventory immediately
-            isInventory = true;
+            inInventory = true;
         }
         else{
-           isInventory = false;
+           inInventory = false;
         }
     }
     public boolean isInventoryOrNot(){
-        return isInventory;
+        return inInventory;
     }
     public boolean isOnMethod(){
         correctLocation = true;
@@ -113,14 +185,18 @@ public class DarkSuit extends Items {
     }
     public boolean isUsed(){
         // If the user uses it on a puzzle, then the puzzle will be skipped
-        if(used){
+        if(isUsed){
+            // this signifies the user used the potion and removes it from inventory
+            isUsed = true;
+            inInventory = false;
         }
         else{
-            return false;
+            isUsed = false;
+            inInventory = true;
         } 
         return false;
     }
-    @Override
+ 
     public void upgradeItem(Items i)
     {
         i.setValue(3 * i.getValue());
