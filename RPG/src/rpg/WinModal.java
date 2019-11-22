69ea5c01be4909fd1909a2e2339691046aeab1ea
jeanieherold <jeaniemherold68@gmail.com>
2019-11-21 23:46:29 -0500

    made win modal look better

--- RPG/src/rpg/WinModal.java
+++ RPG/src/rpg/WinModal.java
@@ -10,7 +10,10 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -29,19 +32,24 @@ public class WinModal {
         popupStage.initModality(Modality.APPLICATION_MODAL);
         popupStage.setTitle("Congratulations!");
         
+        Image win = new Image("file:images/win.png");
+        ImageView winV = new ImageView(win);
+        
         Label mess = new Label();
-        mess.setText("Awesome, " + name + " ! You Win!!");
+        mess.setId("winLabel");
+        mess.setTextAlignment(TextAlignment.CENTER);
+        mess.setText("Awesome, " + name + "! \nYou Win!!");
         
         Button ok = new Button("OK");
         //learning lambdas - one line can go right after the arrow - more than one line of code needs to be in curly {}
         ok.setOnAction(e -> popupStage.close());
         
         VBox container = new VBox(10);
-        container.getChildren().addAll(mess, ok);
+        container.getChildren().addAll(mess, winV, ok);
         container.setAlignment(Pos.CENTER);
         container.setPadding(new Insets(25));
         
-        Scene scene = new Scene(container, 300, 200);
+        Scene scene = new Scene(container, 500, 400);
         scene.getStylesheets().add("rpg-styles.css");
         popupStage.setScene(scene);
         popupStage.showAndWait();
